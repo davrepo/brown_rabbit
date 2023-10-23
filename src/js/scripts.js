28e71ece89e28cecebfce41f5b6b98c79fd9b2ac
volcanodoryshrimpnettuba <111703011+volcanodoryshrimpnettuba@users.noreply.github.com>
2023-10-23 18:30:10 +0200

    readmore.html

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,14 +1,59 @@
-window.onload = function () {
+const sponsorsContainer = document.getElementById('sponsors');
 
-    var el = document.createElement("div");
-    el.innerHTML = "RABBIT TEST ";
-    var div = document.getElementById("headline");
-    insertAfter(div, el);
+function createSponsorRow(start, end) {
+    const row = document.createElement('div');
+    row.className = 'row';
 
-    /* FUNCTIONS */
+    for (let i = start; i <= end; i++) {
+        const col = document.createElement('div');
+        col.className = 'col-1';
 
-    // Inserts newNode after referenceNode
-    function insertAfter(referenceNode, newNode) {
-        referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
+        const img = document.createElement('img');
+        img.src = `assets/images/sponsors/sponsor-${i}.png`;
+        img.alt = `Sponsor ${i}`;
+        img.className = 'img-fluid';
+
+        col.appendChild(img);
+        row.appendChild(col);
     }
-};
+
+    return row;
+}
+
+sponsorsContainer.appendChild(createSponsorRow(1, 10));
+sponsorsContainer.appendChild(createSponsorRow(11, 20));
+
+// JavaScript to create the 3x3 grid of images
+const instagramFeed = document.getElementById('instagram-feed');
+const rows = instagramFeed.getElementsByClassName('instagram-row');
+
+for (let i = 0; i < 9; i++) {
+    const img = document.createElement('img');
+    img.src = `assets/images/instagram-feed/instagram-image-${i + 1}.png`;
+    img.alt = `Instagram Image ${i + 1}`;
+    img.className = 'img-fluid';
+
+    const rowIndex = Math.floor(i / 3);
+    rows[rowIndex].appendChild(img);
+}
+
+// readMore button functionality
+const readMoreButtons = document.querySelectorAll('.read-more-btn');
+    const popupContainer = document.getElementById('popup-container');
+    const popupContent = document.getElementById('popup-content');
+
+readMoreButtons.forEach(button => {
+    button.addEventListener('click', function() {
+        fetch('readMore.html')
+            .then(response => response.text())
+            .then(data => {
+                popupContent.innerHTML = data;
+                popupContainer.style.display = 'flex';
+            })
+            .catch(error => console.log(error));
+    });
+});
+
+popupContainer.addEventListener('click', function() {
+    popupContainer.style.display = 'none';
+});
